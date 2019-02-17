cad0037e612cd3571b4434e31e34b0d8998712e3
pietmichal <me@michal.cafe>
2019-02-17 11:29:45 +0000

    useStyle tests + first CI setup

--- src/use-style-hook/use-style-hook.test.ts
+++ src/use-style-hook/use-style-hook.test.ts
@@ -1,18 +1,68 @@
+import { testHook } from "react-testing-library";
 import { useStyle } from "./use-style-hook";
 
 describe("useStyle hook", () => {
-  xit("returns a style map", () => {
-    const style = useStyle(`
+  it("returns a style map", () => {
+    const input = `
       .foo {
         color: red;
       }
       .bar {
         font-weight: bold;
       }
-    `);
+    `;
+
+    let style: any;
+    testHook(() => (style = useStyle(input)));
+
     expect(Object.keys(style)).toEqual(["foo", "bar"]);
-    expect(Object.entries(style)).toEqual(["foo", "bar"]);
+    expect(Object.values(style)).not.toEqual(["foo", "bar"]);
   });
-  xit("returns the same style map on subsequent calls when style is the same", () => {});
-  xit("returns different style map on subsequent calls when style changes", () => {});
+
+  it("returns the same style map on subsequent calls when style is the same", () => {
+    const input = `
+      .foo {
+        color: red;
+      }
+      .bar {
+        font-weight: bold;
+      }
+    `;
+
+    let style: any;
+    const component = testHook(() => (style = useStyle(input)));
+
+    const expectedStyle = { ...style };
+    style = null;
+
+    component.rerender();
+
+    expect(style).toEqual(expectedStyle);
+  });
+
+  it("returns different style map on subsequent calls when style changes", () => {
+    let color = "red";
+    let fontWeight = "bold";
+    const input = `
+      .foo {
+        color: ${color};
+      }
+      .bar {
+        font-weight: ${fontWeight};
+      }
+    `;
+
+    let style: any;
+    const component = testHook(() => (style = useStyle(input)));
+
+    const expectedStyle = { ...style };
+    style = null;
+
+    color = "green";
+    fontWeight = "normal";
+
+    component.rerender();
+
+    expect(style).not.toEqual(expectedStyle);
+  });
 });
